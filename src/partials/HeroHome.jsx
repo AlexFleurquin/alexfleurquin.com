ef058c20d6178b1ef59985b76ddc19a76360f3cf
Alexandre Fleurquin <alexfleurquin@MacBook-Air-de-Morgane.local>
2022-06-20 20:14:56 +0200

    Release 1.0.3

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -33,21 +33,36 @@ function HeroHome() {
             
             </h1>
           <p className="text-xl text-gray-400 font-display" data-aos="fade-up" data-aos-delay="200">Product Manager</p>
-              
+            {/* CTA  */}
+          <div className="max-w-xs py-10 mx-auto sm:max-w-none sm:flex sm:justify-center">
+                <div data-aos="fade-up" data-aos-delay="400">
+                  <a className="btn rounded-md drop-shadow text-white dark:text-black bg-blue-300 dark:bg-white hover:bg-blue-200 dark:hover:bg-gray-100 w-full mb-4 sm:w-auto sm:mb-0" href="../pdf/Resume_Alexandre_Fleurquin.pdf">Télécharger mon CV
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+                <div data-aos="fade-up" data-aos-delay="600">
+                  
+                  <a className="btn rounded-md bg-transparent border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white w-full sm:w-auto sm:ml-4" href="#0">Me contacter
+                  <svg class="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                      fill-rule="evenodd"
+                      d="M6.672 1.911a1 1 0 10-1.932.518l.259.966a1 1 0 001.932-.518l-.26-.966zM2.429 4.74a1 1 0 10-.517 1.932l.966.259a1 1 0 00.517-1.932l-.966-.26zm8.814-.569a1 1 0 00-1.415-1.414l-.707.707a1 1 0 101.415 1.415l.707-.708zm-7.071 7.072l.707-.707A1 1 0 003.465 9.12l-.708.707a1 1 0 001.415 1.415zm3.2-5.171a1 1 0 00-1.3 1.3l4 10a1 1 0 001.823.075l1.38-2.759 3.018 3.02a1 1 0 001.414-1.415l-3.019-3.02 2.76-1.379a1 1 0 00-.076-1.822l-10-4z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </a>
+                </div>
+                
+          </div>
         </div>
+
+  
         
         {/* Image */}
         <img className="mx-auto rounded-md" src={HeroImage} width="768" height="432" alt="Hero" data-aos="fade-up" data-aos-delay="400" />
 
-        {/* CTA 
-        <div className="max-w-xs py-10 mx-auto sm:max-w-none sm:flex sm:justify-center">
-              <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-blue-500 hover:bg-blue-400 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Découvrir Jinial</a>
-              </div>
-              <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Me contacter</a>
-              </div>
-        </div> */}
       </div>
 
     </div>
