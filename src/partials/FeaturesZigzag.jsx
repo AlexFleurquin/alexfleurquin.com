eb97e160191873f7976cb8835101e5714c92ce42
Alexandre Fleurquin <alexandre.fleurquin@gmail.com>
2022-11-30 13:52:01 +0100

    Remove folders and useless files

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import FeatImage01 from '../images/features-03-image-01.png';
-import FeatImage02 from '../images/features-03-image-02.png';
-import FeatImage03 from '../images/features-03-image-03.png';
-import HeroIllustration from '../images/hero-illustration.png';
 import YarderImage from '../images/yarder-dark.png';
 import JinialImage from '../images/jinial-dark.png';
 import SpotifyImage from '../images/spotify-dark.png';
